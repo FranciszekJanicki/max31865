8416d84c9177d64a8e77dd2456da0d28aa3d2c15
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-06-25 17:49:37 +0200

    Initial commit

---
+++ max31865.h
@@ -0,0 +1,40 @@
+#ifndef MAX31865_MAX31865_H
+#define MAX31865_MAX31865_H
+
+#include "max31865_config.h"
+#include "max31865_registers.h"
+#include <stdbool.h>
+
+typedef struct {
+    max31865_config_t config;
+    max31865_interface_t interface;
+} max31865_t;
+
+max31865_err_t max31865_initialize(max31865_t* max31865,
+                                   max31865_config_t const* config,
+                                   max31865_interface_t const* interface);
+max31865_err_t max31865_deinitialize(max31865_t* max31865);
+
+max31865_err_t max31865_get_resistance_data_raw(max31865_t const* max31865, uint16_t* raw);
+max31865_err_t max31865_get_resistance_data_scaled(max31865_t const* max31865, float32_t* scaled);
+
+max31865_err_t max31865_set_config_reg(max31865_t const* max31865,
+                                       max31865_config_reg_t const* reg);
+max31865_err_t max31865_get_config_reg(max31865_t const* max31865, max31865_config_reg_t* reg);
+
+max31865_err_t max31865_set_high_fault_reg(max31865_t const* max31865,
+                                           max31865_high_fault_reg_t const* reg);
+max31865_err_t max31865_get_high_fault_reg(max31865_t const* max31865,
+                                           max31865_high_fault_reg_t* reg);
+
+max31865_err_t max31865_set_low_fault_reg(max31865_t const* max31865,
+                                          max31865_low_fault_reg_t const* reg);
+max31865_err_t max31865_get_low_fault_reg(max31865_t const* max31865,
+                                          max31865_low_fault_reg_t* reg);
+
+max31865_err_t max31865_get_rtd_reg(max31865_t const* max31865, max31865_rtd_reg_t* reg);
+
+max31865_err_t max31865_set_vbias(max31865_t const* max31865, bool vbias);
+max31865_err_t max31865_start_one_shot_conversion(max31865_t const* max31865);
+
+#endif // MAX31865_MAX31865_H
---
+++ max31865_registers.h
@@ -0,0 +1,33 @@
+#ifndef MAX31865_MAX31865_REGISTERS_H
+#define MAX31865_MAX31865_REGISTERS_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint8_t vbias : 1;
+    uint8_t conv_mode : 1;
+    uint8_t oneshot : 1;
+    uint8_t nwires : 1;
+    uint8_t fault_detect : 2;
+    uint8_t fault_clear : 1;
+    uint8_t mainsfilter : 1;
+} max31865_config_reg_t;
+
+typedef struct RTD {
+    uint16_t rtd : 15;
+    uint8_t fault : 1;
+} max31865_rtd_reg_t;
+
+typedef struct HIGH_FAULT {
+    uint16_t high_fault : 16;
+} max31865_high_fault_reg_t;
+
+typedef struct LOW_FAULT {
+    uint16_t low_fault : 16;
+} max31865_low_fault_reg_t;
+
+typedef struct FAULT_STATUS {
+    uint8_t fault_status : 8;
+} max31865_fault_status_reg_t;
+
+#endif // MAX31865_MAX31865_REGISTERS_H
