128dfa22a01e071e36739590d0a2cbb2fb9c46e9
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-24 13:55:18 +0200

    changes

--- max31865.h
+++ max31865.h
@@ -15,15 +15,19 @@ max31865_err_t max31865_initialize(max31865_t* max31865,
                                    max31865_interface_t const* interface);
 max31865_err_t max31865_deinitialize(max31865_t* max31865);
 
-max31865_err_t max31865_get_resistance_data_raw(max31865_t const* max31865, uint16_t* raw);
-max31865_err_t max31865_get_resistance_data_scaled(max31865_t const* max31865, float32_t* scaled);
+max31865_err_t max31865_get_resistance_data_raw(max31865_t const* max31865,
+                                                uint16_t* raw);
+max31865_err_t max31865_get_resistance_data_scaled(max31865_t const* max31865,
+                                                   float32_t* scaled);
 
 max31865_err_t max31865_set_config_reg(max31865_t const* max31865,
                                        max31865_config_reg_t const* reg);
-max31865_err_t max31865_get_config_reg(max31865_t const* max31865, max31865_config_reg_t* reg);
+max31865_err_t max31865_get_config_reg(max31865_t const* max31865,
+                                       max31865_config_reg_t* reg);
 
-max31865_err_t max31865_set_high_fault_reg(max31865_t const* max31865,
-                                           max31865_high_fault_reg_t const* reg);
+max31865_err_t max31865_set_high_fault_reg(
+    max31865_t const* max31865,
+    max31865_high_fault_reg_t const* reg);
 max31865_err_t max31865_get_high_fault_reg(max31865_t const* max31865,
                                            max31865_high_fault_reg_t* reg);
 
@@ -32,7 +36,8 @@ max31865_err_t max31865_set_low_fault_reg(max31865_t const* max31865,
 max31865_err_t max31865_get_low_fault_reg(max31865_t const* max31865,
                                           max31865_low_fault_reg_t* reg);
 
-max31865_err_t max31865_get_rtd_reg(max31865_t const* max31865, max31865_rtd_reg_t* reg);
+max31865_err_t max31865_get_rtd_reg(max31865_t const* max31865,
+                                    max31865_rtd_reg_t* reg);
 
 max31865_err_t max31865_set_vbias(max31865_t const* max31865, bool vbias);
 max31865_err_t max31865_start_one_shot_conversion(max31865_t const* max31865);
